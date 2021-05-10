f3b0a5aeb9ee96a6237ddb021e44d327366de656
ADOLLAR1 <adollar@stcharlessd.org>
2021-05-10 07:58:52 -0500

    Changed footer

--- scripts/footer.js
+++ scripts/footer.js
@@ -11,3 +11,33 @@ let year = date.getFullYear();
 let str = `&copy; ${year.toString()} Alex Dollar`
 
 copy.innerHTML = str;
+
+//WAIT SO PAGE CAN LOAD
+
+{
+    let i = 0;
+    setInterval(() => {
+        if (i === 1) {
+            const foot = document.querySelector("#footer");
+
+            const ch = document.documentElement.scrollHeight;
+            const vwh = document.documentElement.clientHeight;
+            document.documentElement.style.height = "100%";
+            const h = Math.max(ch, vwh);
+
+            console.log(ch);
+            console.log(vwh);
+            console.log(h);
+
+            const height = foot.scrollHeight;
+
+            if (h === vwh) {
+                foot.style.position = 'absolute';
+                foot.style.bottom = `1em`;
+            }
+        }
+        if (i <= 2) {
+            i++;
+        }
+    }, 10);
+}
