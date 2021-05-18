55824ae0ebdebc4cdb421ce3161abac6aae43149
ADOLLAR1 <adollar@stcharlessd.org>
2021-05-18 08:45:20 -0500

    Added email game

--- scripts/games.js
+++ scripts/games.js
@@ -1,7 +1,32 @@
 let clickCounter = 0;
 
+let clickers = 0;
+
+let element;
+
+function setElement(ele) {
+    element = ele;
+}
 
 function clicker(element) {
     clickCounter++;
     element.innerHTML = clickCounter;
+}
+
+function update(element) {
+    element.innerHTML = clickCounter;
+}
+
+function purchaseCheck() {
+    if (clickCounter >= 10) {
+        clickCounter -= 10;
+        clickers++;
+    }
+}
+
+function startClickers() {
+    setInterval(() => {
+        clickCounter += clickers;
+        update(element);
+    }, 1000);
 }
